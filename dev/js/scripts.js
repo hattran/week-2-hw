edac1aa93d00e4123dfa870f808891f465b1e3c8
hattran <63318539+hattran@users.noreply.github.com>
2020-05-12 22:37:52 -0400

    updated folders

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,22 @@
 
+var $burger = $("#burger-icon");
+var $nav = $('nav[data-nav]');
+var $navA = $('nav[data-nav] a');
+var isVisible = false;
+
+function mouseClick(){
+
+    console.log("click");
+    if(isVisible === false){
+        $nav.show();
+        isVisible = true;
+    }
+    else{
+        $nav.hide();
+        isVisible = false;
+    }
+}
+
+$burger.on( "click", mouseClick);
+// close the mobile menu when menu link is clicked
+$navA.on( "click", mouseClick);
